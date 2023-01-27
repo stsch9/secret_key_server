9249aa562322e575e53037e3ef290e37260646f7
stsch9 <s.schaubeck@dracoon.com>
2023-01-27 18:21:43 +0100

    add further functions in opaque

--- oprf/opaque.py
+++ oprf/opaque.py
@@ -1,12 +1,77 @@
 # https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-opaque
 
-from oprf_ristretto25519_sha512 import Blind, DeriveKeyPair, BlindEvaluate
-from cryptography.hazmat.primitives import hashes
+from oprf_ristretto25519_sha512 import Blind, DeriveKeyPair, BlindEvaluate, Finalize
+from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.kdf.hkdf import HKDFExpand
+from nacl.utils import random
 
 # Global Variables
 
 Nok = 32
+Nn = 32
+Nh = 64
+Nseed = 32
+
+
+def encode_vector_len(data: bytes, L: int) -> bytes:
+    return len(data).to_bytes(L, 'big') + data
+
+
+def encode_vector(data: bytes) -> bytes:
+    return encode_vector_len(data, 2)
+
+# 2.2 Key Derivation Function and Message Authentication Code
+
+# https://www.rfc-editor.org/rfc/rfc5869
+def Extract(salt: bytes, ikm: bytes) -> bytes:
+    h = hmac.HMAC(salt, hashes.SHA512())
+    h.update(ikm)
+    signature = h.finalize()
+    return signature
+
+def Expand(key_material: bytes, info: bytes, length: int) -> bytes:
+    hkdf = HKDFExpand(
+        algorithm=hashes.SHA512(),
+        length=length,
+        info=info,
+    )
+    return hkdf.derive(key_material)
+
+# 4 Client Credential Storage and Key Recovery
+
+def CreateCleartextCredentials(server_public_key: bytes, client_public_key: bytes,
+                               server_identity=b'', client_identity=b'') -> bytes:
+  # Set identities as public keys if no application-layer identity is provided
+    if len(server_identity) == 0:
+        server_identity = server_public_key
+    if len(client_identity) == 0:
+        client_identity = client_public_key
+
+  #Create CleartextCredentials cleartext_credentials with
+  #  (server_public_key, server_identity, client_identity)
+    #return server_public_key + server_identity + client_identity
+
+    return server_public_key + encode_vector(server_identity) + encode_vector(client_identity)
+
+# 4.1.2. Envelope Creation
+
+def Store(randomized_pwd: bytes, server_public_key: bytes, server_identity=b'', client_identity=b'') -> tuple[bytes, bytes, bytes, bytes,]:
+    envelope_nonce = random(Nn)
+    masking_key = Expand(randomized_pwd, b"MaskingKey", Nh)
+    auth_key = Expand(randomized_pwd, envelope_nonce + b"AuthKey", Nh)
+    export_key = Expand(randomized_pwd, envelope_nonce + b"ExportKey", Nh)
+    seed = Expand(randomized_pwd, envelope_nonce + b"PrivateKey", Nseed)
+    _, client_public_key = DeriveKeyPair(seed, b"OPAQUE-DeriveAuthKeyPair")
+
+    cleartext_creds = CreateCleartextCredentials(server_public_key, client_public_key,
+                               server_identity, client_identity)
+    h = hmac.HMAC(auth_key, hashes.SHA512())
+    h.update(envelope_nonce + cleartext_creds)
+    auth_tag = h.finalize()
+
+    #Create Envelope envelope with (envelope_nonce, auth_tag)
+    envelope = envelope_nonce + auth_tag
+    return envelope, client_public_key, masking_key, export_key
 
 #  5.2. Registration Functions
 
@@ -21,14 +86,8 @@ def CreateRegistrationRequest(password: bytes) -> tuple[bytes, bytes]:
 #def CreateRegistrationResponse(request, server_public_key: bytes,
 #                               credential_identifier, oprf_seed):
 def CreateRegistrationResponse(blinded_message: bytes, server_public_key: bytes,
-                               credential_identifier: bytes, oprf_seed: bytes) -> bytes:
-
-    Expand = HKDFExpand(
-        algorithm=hashes.SHA512(),
-        length=Nok,
-        info=credential_identifier + b"OprfKey"
-    )
-    seed = Expand.derive(oprf_seed)
+                               credential_identifier: bytes, oprf_seed: bytes) -> tuple[bytes, bytes]:
+    seed = Expand(oprf_seed, credential_identifier + b"OprfKey", Nok)
     oprf_key, _ = DeriveKeyPair(seed, b"OPAQUE-DeriveKeyPair")
 
     # blinded_element = DeserializeElement(request.blinded_message)
@@ -40,3 +99,21 @@ def CreateRegistrationResponse(blinded_message: bytes, server_public_key: bytes,
 
     # Create RegistrationResponse response with (evaluated_message, server_public_key)
     return evaluated_message, server_public_key
+
+
+#def FinalizeRegistrationRequest(password, blind, response, server_identity, client_identity):
+def FinalizeRegistrationRequest(password: bytes, blind: bytes, evaluated_message: bytes, server_public_key: bytes,
+                                server_identity=b'', client_identity=b'') -> tuple[bytes, bytes, bytes, bytes]:
+    # evaluated_element = DeserializeElement(response.evaluated_message)
+    evaluated_element = evaluated_message
+    oprf_output = Finalize(password, blind, evaluated_element)
+    print("oprf_output: " + oprf_output.hex())
+
+    # stretched_oprf_output = Stretch(oprf_output, params)
+    stretched_oprf_output = oprf_output
+    randomized_pwd = Extract(b'', oprf_output + stretched_oprf_output)
+
+    envelope, client_public_key, masking_key, export_key = \
+        Store(randomized_pwd, server_public_key, server_identity, client_identity)
+    #Create RegistrationRecord record with (client_public_key, masking_key, envelope)
+    return client_public_key, masking_key, envelope, export_key
--- oprf/test_opaque.py
+++ oprf/test_opaque.py
@@ -1,34 +1,74 @@
-from opaque import CreateRegistrationRequest, CreateRegistrationResponse
-from oprf_ristretto25519_sha512 import DeriveKeyPair
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.hkdf import HKDFExpand
+from opaque import CreateRegistrationRequest, CreateRegistrationResponse, CreateCleartextCredentials, Expand, Extract
+from oprf_ristretto25519_sha512 import DeriveKeyPair, Finalize
 from oblivious import sodium
+from cryptography.hazmat.primitives import hashes, hmac
+
+# adapted functions
+
+def Store(randomized_pwd: bytes, server_public_key: bytes, server_identity=b'', client_identity=b'') -> tuple[bytes, bytes, bytes, bytes,]:
+    #envelope_nonce = random(Nn)
+    envelope_nonce = bytes.fromhex('ac13171b2f17bc2c74997f0fce1e1f35bec6b91fe2e12dbd323d23ba7a38dfec')
+    masking_key = Expand(randomized_pwd, b"MaskingKey", Nh)
+    auth_key = Expand(randomized_pwd, envelope_nonce + b"AuthKey", Nh)
+    print('auth_key: ' + auth_key.hex())
+    export_key = Expand(randomized_pwd, envelope_nonce + b"ExportKey", Nh)
+    seed = Expand(randomized_pwd, envelope_nonce + b"PrivateKey", Nseed)
+    _, client_public_key = DeriveKeyPair(seed, b"OPAQUE-DeriveAuthKeyPair")
+
+    cleartext_creds = CreateCleartextCredentials(server_public_key, client_public_key, server_identity, client_identity)
+    h = hmac.HMAC(auth_key, hashes.SHA512())
+    h.update(envelope_nonce + cleartext_creds)
+    auth_tag = h.finalize()
+
+    #Create Envelope envelope with (envelope_nonce, auth_tag)
+    envelope = envelope_nonce + auth_tag
+    return envelope, client_public_key, masking_key, export_key
+
+
+def FinalizeRegistrationRequest(password: bytes, blind: bytes, evaluated_message: bytes, server_public_key: bytes,
+                                server_identity=b'', client_identity=b'') -> tuple[bytes, bytes, bytes, bytes]:
+    evaluated_element = evaluated_message
+    oprf_output = Finalize(password, blind, evaluated_element)
+    print("oprf_output: " + oprf_output.hex())
+    stretched_oprf_output = oprf_output
+    randomized_pwd = Extract(b'', oprf_output + stretched_oprf_output)
+    print('randomized_pwd: ' + randomized_pwd.hex())
+
+    envelope, client_public_key, masking_key, export_key = \
+        Store(randomized_pwd, server_public_key, server_identity, client_identity)
+    return client_public_key, masking_key, envelope, export_key
+
+# Input Values
 
 Nok = 32
+Nn = 32
+Nh = 64
+Nseed = 32
 
 password = '436f7272656374486f72736542617474657279537461706c65'
 server_private_key = '47451a85372f8b3537e249d7b54188091fb18edde78094b43e2ba42b5eb89f0d'
 server_public_key = sodium.bas(bytes.fromhex(server_private_key))
 oprf_seed = 'f433d0227b0b9dd54f7c4422b600e764e47fb503f1f9a0f0a47c6606b054a7fdc65347f1a08f277e22358bbabe26f823fca82c7848e9a75661f4ec5d5c1989ef'
 credential_identifier = '31323334'
+envelope_nonce = 'ac13171b2f17bc2c74997f0fce1e1f35bec6b91fe2e12dbd323d23ba7a38dfec'
+server_identity = ''
+client_identity = ''
+
 
 print("server_public_key: " + server_public_key.hex())
 
 blinded_message, blind = CreateRegistrationRequest(bytes.fromhex(password))
 
 # derive oprf_key
-Expand = HKDFExpand(
-        algorithm=hashes.SHA512(),
-        length=Nok,
-        info=bytes.fromhex(credential_identifier) + b"OprfKey"
-)
-seed = Expand.derive(bytes.fromhex(oprf_seed))
+seed = Expand(bytes.fromhex(oprf_seed), bytes.fromhex(credential_identifier) + b"OprfKey", Nok)
 oprf_key, _ = DeriveKeyPair(seed, b"OPAQUE-DeriveKeyPair")
-
 print("oprf_key: " + oprf_key.hex())
 
 evaluated_message, server_public_key = CreateRegistrationResponse(blinded_message, server_public_key,
                                                                   bytes.fromhex(credential_identifier),
                                                                   bytes.fromhex(oprf_seed))
 
-
+client_public_key, masking_key, envelope, export_key = FinalizeRegistrationRequest(bytes.fromhex(password), blind, evaluated_message, server_public_key, server_identity=bytes.fromhex(server_identity), client_identity=bytes.fromhex(client_identity))
+print('client_public_key: ' + client_public_key.hex())
+print('export_key: ' + export_key.hex())
+print('envelop: ' + envelope.hex())
